4a267b6a0093416364149b7e97e4da3d907d7732
Virendra Singh <38318324+virendra56422@users.noreply.github.com>
2019-03-29 14:08:22 +0530

    Add missing Semicolon in printf statement

--- add.c
+++ add.c
@@ -4,7 +4,7 @@ int main()
 	int a,b=1;
 	scanf("%d %d",&a,&b);
 	a=a+b;
-	printf("Enter 0 when Want to Show Total Sum of All No.\n);
+	printf("Enter 0 when Want to Show Total Sum of All No.\n");
 	while(b)
 	{
 		scanf("%d",&b);
